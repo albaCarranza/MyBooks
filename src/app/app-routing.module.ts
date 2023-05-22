9288410d9afb285f88675c01b791873b5f2850dd
albaCarranza <alba.carranza.g@gmail.com>
2023-05-22 17:57:05 +0200

    subam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormRegisterComponent } from './component/form-register/form-register.component';
+import { RegisterComponent } from './pages/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    FormRegisterComponent,
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
